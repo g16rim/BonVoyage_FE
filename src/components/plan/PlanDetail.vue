1643488ae9714b3d06d35f55346b621960f1b03c
Kyungrim Park <pkl4693@naver.com>
2024-05-21 08:22:50 +0900

    fix: #4 계획 상세

--- src/components/plan/PlanDetail.vue
+++ src/components/plan/PlanDetail.vue
@@ -56,6 +56,33 @@ function onDeletePlan() {
 
 <template>
   <div class="container">
+    <div class="max-w-xl mx-auto bg-white rounded-xl shadow-md overflow-hidden lg:max-w-4xl xl:max-w-6xl">
+      <div class="flex justify-between items-center p-8"> <!-- 여기에 flex와 items-center를 추가 -->
+        <!-- planTitle을 별도의 행으로 추가하고 크기를 조정 -->
+        <div class="text-xl font-bold">{{ plan.planTitle }}</div> <!-- text-xl과 font-bold로 변경 -->
+        <!-- VLeftTime 컴포넌트를 크고 초록색으로 표시하고 크기를 조정 -->
+        <div class="text-xl font-bold" style="color: #32a852;"> <!-- text-xl과 font-bold로 변경 -->
+          <VLeftTime :dday="plan.startDate" />
+        </div>
+      </div>
+      <div class="p-8 flex justify-between">
+        <div>
+          <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black">여행 기간: {{ plan.startDate }} - {{
+            plan.endDate }}</a>
+          <p class="mt-2 text-gray-500">예산: {{ plan.budget }}원</p>
+        </div>
+        <!-- 이미지 추가 -->
+        <img src="/src/assets/loading.gif" alt="설명" class="w-24 h-24 object-cover rounded-full">
+      </div>
+      <!-- 버튼 추가 -->
+      <div class="mt-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">지도</button>
+        <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">수정</button>
+        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">삭제</button>
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">목록</button>
+      </div>
+    </div>
+
     <div class="row mt-5">
       <VLeftTime :dday="plan.startDate" />
       <div class="col-md-12">
